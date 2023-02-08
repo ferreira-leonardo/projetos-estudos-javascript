a131b16d7fbd634e01d8b31fde416e2bb5f9b6f5
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2023-02-08 18:35:40 -0300

    Projeto slideShow finalizado

---
+++ 6.slideshow/assets/script/index.js
@@ -0,0 +1,43 @@
+let containerItems = document.getElementById('container-items'); 
+let btnNext = document.getElementById('next');
+let btnPrevious = document.getElementById('previous');
+
+//Objeto JSON das imagens
+const images = [
+    {'id': '1', 'url' : 'assets/img/chrono.jpg'},
+    {'id': '2', 'url' : 'assets/img/inuyasha.jpg'},
+    {'id': '3', 'url' : 'assets/img/ippo.png'},
+    {'id': '4', 'url' : 'assets/img/tenchi.jpg'},
+    {'id': '5', 'url' : 'assets/img/tenjhotenge.jpg'},
+    {'id': '6', 'url' : 'assets/img/yuyuhakusho.jpg'},
+]
+
+//Função que carrega as imagens
+const loadImages = (images, container) => {
+    images.forEach( img => {
+        container.innerHTML += `
+        <div class='item'> 
+            <img src="${img.url}">
+        </div>`
+    })
+}
+
+loadImages( images, containerItems );
+
+let items = document.querySelectorAll('.item'); //Pegando todas as imagens que foramm geradas
+
+//Função colocar a primeira imagem para o final ao clicarmos no 'next'
+const next = () =>{
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item')
+}
+
+btnNext.addEventListener('click', next);
+
+const previous = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+btnPrevious.addEventListener('click', previous);
